7c4f26a9ad842da9c532d33e20b37fb18f92da49
Jun <87632049+peacekeeper6@users.noreply.github.com>
2022-04-27 07:01:47 +0000

    real final commit (hopefully)  - had to switch input and prompt for the console to not display "none" randomly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ array_no_space_concise_no_uni = ["Keltec", "Premo", "Mantra", "Skulk", "Eclipse"
 array_no_space_no_concise_uni = ["qRekusuki", "RekusukiVz", "RekusukiDFS", "$Rekusuki", "0Rekusuki0", "1017Rekusuki", "Rekusuki3s", "Rekusuki2P", "qSemantic", "SemanticVz", "SemanticDFS", "$Semantic", "0Semantic0", "1017Semantic", "Semantic3s", "Semantic2P", "qPineault", "PineaultVz", "PineaultDFS", "$Pineault", "0Pineault0", "1017Pineault", "Pineault3s", "Pineault2P", "qHalfnHalf", "HalfnHalfVz", "$HalfnHalf", "0HalfnHalf", "1017HalfnHalf", "HalfnHalf3s", "HalfnHalf2P", "qGetBread", "GetBreadVz", "GetBreadDFS", "$GetBread", "0GetBread0", "1017GetBread", "GetBread3s", "GetBread2P", "qDarkiesha", "DarkieshaVz", "DarkieshaDFS", "$Darkiesha", "0Darkiesha0", "1017Darkiesha", "Darkiesha3s", "Darkiesha2P", "qPackrunner", "PackrunnerVz", "PackrunnerDFS", "$Packrunner", "0Packrunner0", "1017Packrunner", "Packrunner3s", "Packrunner2P", "qArsenics", "ArsenicsVz", "ArsenicsDFS", "$Arsenics", "0Arsenics0", "1017Arsenics", "Arsenics3s", "Arsenics2P", "qPeacekeeper", "PeacekeeperVz", "PeacekeeperDFS", "$Peacekeeper", "0Peacekeeper0", "1017Peacekeeper", "Peacekeeper3s", "Peacekeeper2P", "qSpriteTech", "SpriteTechVz", "SpriteTechDFS", "$SpriteTech", "0SpriteTech0", "1017SpriteTech", "SpriteTech3s", "SpriteTech2P", "qIndominus", "IndominusVz", "IndominusDFS", "$Indominus", "0Indominus0", "1017Indominus", "Indominus3s", "Indominus2P", "qBludgeon", "BludgeonVz", "BludgeonDFS", "$Bludgeon", "0Bludgeon0", "1017Bludgeon", "Bludgeon3s", "Bludgeon2P", "qChromium", "ChromiumVz", "ChromiumDFS", "$Chromium", "0Chromium0", "1017Chromium", "Chromium3s", "Chromium2P", "qLeviathan", "LeviathanVz", "LeviathanDFS", "$Leviathan", "0Leviathan0", "1017Leviathan", "Leviathan3s", "Leviathan2P", "qPenultimate", "PenultimateVz", "PenultimateDFS", "$Penultimate", "0Penultimate0", "1017Penultimate", "Penultimate3s", "Penultimate2P", "qBullwhip", "BullwhipVz", "BUllwhipDFS", "$Bullwhip", "0Bullwhip0", "1017Bullwhip", "Bullwhip3s", "Bullwhip2P", "qJubilees", "JubileesVz", "JubileesDFS", "$Jubilees", "0Jubilees0", "1017Jubilees", "Jubilees3s", "Jubilees2P", "qAutomatic", "AutomaticVz", "AutomaticDFS", "Automatic", "0Automatic", "1017Automatic", "Automatic3s", "Automatic2P", "qAudacity", "AudacityVz", "AudacityDFS", "$Audacity", "0Audacity0", "1017Audacity", "Audacity3s", "Audacity2P", "qCraniacs", "CraniacsVz", "CraniacsDFS", "$Craniacs", "0Craniacs0", "1017Craniacs", "Craniacs3s", "Craniacs2P"] #160 items
 array_no_space_no_concise_no_uni = ["Rekusuki", "Semantic", "Pineault", "HalfnHalf", "GetBread", "Darkiesha", "Packrunner", "Arsenics", "Peacekeeper", "SpriteTech", "Indominus", "Bludgeon", "Chromium", "Leviathan", "Penultimate", "Bullwhip", "Jubilees", "Automatic", "Audacity", "Craniacs" ] #20 items
 array_space_uni = ["qDouble G", "Double GVz", "Double GDFS", "$Double G", "0Double G0", "1017Double G", "Double G3s", "Double G2P", "qElite Ammo", "Elite AmmoVz", "Elite AmmoDFS", "$Elite Ammo", "0Elite Ammo", "1017EliteAmmo", "EliteAmmo3s", "EliteAmmo2P", "qKill Switch", "Kill SwitchVz", "Kill SwitchDFS", "$Kill Switch", "0Kill Switch", "1017Kill Switch", "Kill Switch3s", "Kill Switch2P", "qDeath Stalker", "Death StalkerVz", "Death StalkerDFS", "$Death Stalker", "0Death Stalker0", "1017Death Stalker", "Death Stalker3s", "Death Stalker2P", "qDigital Enigma", "Digital EnigmaVz", "Digital EnigmaDFS", "$Digital Enigma", "0Digital Enigma0", "1017Digital Enigma", "Digital Enigma3s", "Digital Enigma2P", "qOG Casaonova", "OG CasanovaVz", "OG CasanovaDFS", "$OG Casanova", "1017OG Casanova", "OG Casanova3s", "OG Casanova2P", "qTinfoil Hat", "Tinfoil HatVz", "Tinfoil HatDFS", "$Tinfoil Hat", "0Tinfoil Hat0", "1017Tinfoil Hat", "Tinfoil Hat3s", "Tinfoil Hat2P", "qCough Syrup", "Cough SyrupVz", "Cough SyrupDFS", "$Cough Syrup", "0Cough Syrup0", "1017Cough Syrup", "Cough Syrup3s", "Cough Syrup2P", "qNight Watcher", "Night WatcherVz", "Night WatcherDFS", "$Night Watcher", "0Night Watcher0", "1017Night Watcher", "Night Watcher3s", "Night Watcher2P", "qUndead Alive", "Undead AliveVz", "Undead AliveDFS", "$Undead Alive", "0Undead Alive0", "1017Undead Alive", "Undead Alive3s", "Undead Alive2P", "qWorst Nightmare", "Worst NightmareVz", "Worst NightmareDFS", "$Worst Nightmare", "0Worst Nightmare0", "1017Worst Nightmare", "Worst Nightmare3s", "Worst Nightmare2P", "qShort Fury", "Short FuryVz", "Short FuryDFS", "$Short Fury", "0Short Fury0", "1017Short Fury", "Short Fury3s", "Short Fury2P", "qMolly Wap", "Molly WapVz", "Molly WapDFS", "$Molly Wap", "0Molly Wap0", "1017Molly Wap", "Molly Wap3s", "Molly Wap2P", "qBlank Space", "Blank SpaceVz", "Blank SpaceDFS", "$Blank Space", "0Blank Space0", "1017Blank Space", "Blank Space3s", "Blank Space2P", "qBag Chaser", "Bag ChaserVz", "Bag ChaserDFS", "$Bag Chaser", "0Bag Chaser0", "1017Bag Chaser", "Bag Chaser3S", "Bag Chaser2P", "qThe Reaper", "The ReaperVz", "The ReaperDFS", " $The Reaper", "0The Reaper0", "1017The Reaper", "The Reaper3s", "The Reaper2P", "qFalse Hope", "False HopeVz", "False HopeDFS", "$False Hope", "0False Hope0", "1017False Hope", "False Hope3s", "False Hope2P", "qFlash Bang", "Flash BangVz", "Flash BangDFS", "$Flash Bang", "0Flash Bang0", "1017Flash Bang", "Flash Bang3s", "Flash Bang2P", "qPoint Blank", "Point BlankVz", "Point BlankDFS", "$Point Blank", "0Point Blank0", "1017Point Blank", "Point Blank3s", "Point Blank2P", "qCross Hair", "Cross HairVz", "Cross HairDFS", "$Cross Hair", "0Cross Hair0", "1017Cross Hair", "Cross Hair3s", "Cross Hair2P"] #160 items
-array_space_no_uni = ["Double G", "Elite Ammo", "Kill Switch", "Death Stalker", "Digital Enigma", "OG Casanova", "Tinfoil Hat", "Cough Syrup", "Night Watcher", "Undead Alive", "Worst Nightmare", "Short Fury", "Molly Wap", "Blank Space", "Bag Chaser", "The Reaper", "False Hope" "Flash Bang", "Point Blank" "Cross Hair"] #20 items
+array_space_no_uni = ["Double G", "Elite Ammo", "Kill Switch", "Death Stalker", "Digital Enigma", "OG Casanova", "Tinfoil Hat", "Cough Syrup", "Night Watcher", "Undead Alive", "Worst Nightmare", "Short Fury", "Molly Wap", "Blank Space", "Bag Chaser", "The Reaper", "False Hope", "Flash Bang", "Point Blank", "Cross Hair"] #20 items
 
 while True: 
   def prompt(text):
@@ -17,7 +17,7 @@ while True:
       sys.stdout.flush()
       time.sleep(.03)
       
-  prompt("Welcome to the Random Username Generator! Not all usernames are created equal. Please reroll as needed and enjoy! A Y/N system will be put into place. Y for Yes and N for No. Type Y or N to continue and notion that you understand.") # this prompt is necessary to implement break and start process
+  prompt("Welcome to the Random Username Generator! Make sure that all prompts are loaded before responses are given. Not all usernames are created equal. Please reroll as needed and enjoy! A Y/N system will be put into place. Y for Yes and N for No. Type Y or N to continue and notion that you understand.") # this prompt is necessary to implement break and start process
   time.sleep(0.5)
   choice = input("\n" + "Y/N: ")
   try:
@@ -30,11 +30,11 @@ while True:
       break
   except AssertionError:
     prompt("Please enter Y/y or N/n" + "\n")
+    quit()
 
 def no_space_concise_uni(): #respective results of usernames placed in different orders due to needing to be defined before being called
   prompt("Here is your username, thanks for playing: ")
   prompt(random.choice(array_no_space_concise_uni))
-  # prompt(array_no_space_concise_uni[random.randint(0,200)])
   time.sleep(0.5)
   input("\n" + "Hit enter if you want to reroll your username. Duplicates may occur.")
   os.system('clear')
@@ -42,7 +42,7 @@ def no_space_concise_uni(): #respective results of usernames placed in different
 
 def no_space_concise_no_uni():
   prompt("Here is your username, thanks for playing: ")
-  prompt(array_no_space_concise_no_uni[random.randint(0,20)])
+  prompt(random.choice(array_no_space_concise_no_uni))
   time.sleep(0.5)
   input("\n" + "Hit enter if you want to reroll your username. Duplicates may occur.")
   os.system('clear')
@@ -50,7 +50,7 @@ def no_space_concise_no_uni():
 
 def no_space_no_concise_uni():
   prompt("Here is your username, thanks for playing: ")
-  prompt(array_no_space_no_concise_uni[random.randint(0,160)])
+  prompt(random.choice(array_no_space_no_concise_uni))
   time.sleep(0.5)
   input("\n" + "Hit enter if you want to reroll your username. Duplicates may occur.")
   os.system('clear')
@@ -58,7 +58,7 @@ def no_space_no_concise_uni():
 
 def no_space_no_concise_no_uni():
   prompt("Here is your username, thanks for playing: ")
-  prompt(array_no_space_no_concise[random.randint(0,20)])
+  prompt(random.choice(array_no_space_no_concise))
   time.sleep(0.5)
   input("\n" + "Hit enter if you want to reroll your username. Duplicates may occur.")
   os.system('clear')
@@ -66,7 +66,7 @@ def no_space_no_concise_no_uni():
 
 def space_uni():
   prompt("Here is your username, thanks for playing: ")
-  prompt(array_space_uni[random.randint(0,160)])
+  prompt(random.choice(array_space_uni))
   time.sleep(0.5)
   input("\n" + "Hit enter if you want to reroll your username. Duplicates may occur.")
   os.system('clear')
@@ -74,7 +74,7 @@ def space_uni():
   
 def space_no_uni():
   prompt("Here is your username, thanks for playing: ")
-  prompt(array_space_no_uni[random.randint(0,20)])
+  prompt(random.choice(array_space_no_uni))
   time.sleep(0.5)
   input("\n" + "Hit enter if you want to reroll your username. Duplicates may occur.")
   os.system('clear')
